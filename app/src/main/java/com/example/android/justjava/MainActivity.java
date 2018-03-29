e7be40f5aa8ff9f1a55044c747bde68671a7b865
Maria Kovaleva <31738984+mariakovaleva@users.noreply.github.com>
2018-03-29 17:41:35 +0200

    Lesson 7 quiz 6

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -16,23 +16,21 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
-
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+    String creamStatus;
+    String chocoStatus;
+    int pricePerCup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    int quantity = 0;
-<<<<<<< HEAD
-    String creamStatus;
-=======
-    Context context;
 
     /**
      * This method is called when the + button is clicked.
@@ -51,64 +49,113 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is called when the order button is clicked.
+     * Alternative solution to adding a Whipped Cream checkbox
+     * Checks if the Whipped cream box is selected and summons OrderSummary when clicking order button
+
+     public void whippedCreamChecked(View view){
+     boolean creamOrdered = ((CheckBox) view).isChecked();
+     switch (view.getId())
+     {
+     case R.id.checkbox:
+     if (creamOrdered) creamStatus = "ordered";
+     else creamStatus = "none";
+     break;
+
+     }
+
+     displayMessage(createOrderSummary());
+     }
      */
-
-    public void submitOrder(View view) {
-        int price = quantity*5;
-        Log.v("MainActivity", "The price is: "+price);
-
-        String priceMessage = "Name: Lyla the Labyrinth";
-        priceMessage += "\nQuantity: "+ quantity;
-        priceMessage += "\nTotal: $"+price;
-        priceMessage += "\nWhipped Cream: "+creamStatus;
-        priceMessage += "\nThank you!";
-        displayMessage(priceMessage);
-}
-
-    public void whippedCreamChecked(View view){
-        boolean creamOrdered = ((CheckBox) view).isChecked();
-        switch (view.getId())
-        {
-            case R.id.checkbox:
-                if (creamOrdered) creamStatus = "ordered";
-                else creamStatus = "none";
-                        break;
-
-        }
 
     /**
      * Calculates the price of the order.
-     *  @param quantity is the number of cups of coffee ordered
+     *
+     * @param quantity    is the number of cups of coffee ordered
      * @param pricePerCup is the price of the cup of coffee
-     * */
+     */
+    private int calculatePrice(int quantity, int pricePerCup) {
+        return quantity * pricePerCup;
+    }
+
+    /**
+     * This method is called when the order button is clicked
+     */
+
+    public void submitOrder(View view) {
+
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        if (hasWhippedCream == true) {
+
+            creamStatus = "Yes";
+        } else {
+
+            creamStatus = "No";
+
+        }
+        Log.i("MainActivity", "User chose whipped cream: " + hasWhippedCream);
+
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.choco_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+        if (hasChocolate == true) {
+
+            chocoStatus = "Yes";
+        } else {
+
+            chocoStatus = "No";
+
+        }
+        Log.i("MainActivity", "User chose chocolate: " + hasChocolate);
+
+        int price = calculatePrice(quantity, pricePerCup);
+
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        displayMessage(priceMessage);
+
+    }
 
     /**
      * Creates the summary of the order.
+     *
+     * @param price        of the order
+     * @param whippedCream is whether the user wants whipped cream topping
+     *                     @param chocolate is whether the user wants chocolate topping
+     * @return text summary
      */
->>>>>>> 14e26106598eb87ffd6e953db7f36772682b7522
+    private String createOrderSummary(int price, boolean whippedCream, boolean chocolate) {
+        //how to get calculatePrice's value directly?
+        String orderSummaryMessage = "Name: Maria Kovaleva";
+        orderSummaryMessage += "\nQuantity: " + quantity;
+        orderSummaryMessage += "\nTotal: $" + calculatePrice(quantity, 5);
+        orderSummaryMessage += "\nThank you!";
+        orderSummaryMessage += "\nAdd whipped cream? " + creamStatus;
+        orderSummaryMessage += "\nAdd chocolate? " + chocoStatus;
+        return orderSummaryMessage;
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
+
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int money) {
+    private void display(int quantity) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + money);
-            }
+        quantityTextView.setText("" + quantity);
+    }
 
     /**
      * This method displays the given price on the screen.
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+     private void displayPrice(int number) {
+     TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+     priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+     }
      */
 }
 
